5c6c546e18a24927bb702db23d025399c5ac21aa
KYakut35 <kyakut35@gmail.com>
2025-01-08 17:14:16 +0300

    ChromeDriver and GeckoDriver added

--- src/test/java/com/qework/driver/BaseTest.java
+++ src/test/java/com/qework/driver/BaseTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
@@ -14,20 +18,25 @@ import java.util.concurrent.TimeUnit;
 @ExtendWith(TestResultsLogger.class)
 public class BaseTest  {
 
-    public static WebDriver driver;
-    public static WebDriverWait wait;
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+
+    protected static DesiredCapabilities capabilities;
+    protected static ChromeOptions chromeOptions;
+    FirefoxOptions firefoxOptions;
+
     public static Log logger = new Log();
 
     @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
-        String baseURL = "https://www.vatanbilgisayar.com/";
+        String baseURL = "https://catchylabs-webclient.testinium.com/";
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--disable-gpu");
         chromeOptions.addArguments("--disable-popup-blocking");
         chromeOptions.addArguments("--disable-translate");
-        driver = new ChromeDriver(chromeOptions);
+        driver = new ChromeDriver(chromeOptions());
         wait = new WebDriverWait(driver, 10);
         driver.get(baseURL);
         driver.manage().window().maximize();
@@ -43,5 +52,34 @@ public class BaseTest  {
         driver.quit();
         logger.info("\nTest Scenario Completed.");
     }
+
+    public static ChromeOptions chromeOptions() {
+        chromeOptions = new ChromeOptions();
+        capabilities = DesiredCapabilities.chrome();
+
+        chromeOptions.addArguments("--kiosk");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--start-fullscreen");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        chromeOptions.merge(capabilities);
+        return chromeOptions;
+    }
+
+    public FirefoxOptions firefoxOptions() {
+        firefoxOptions = new FirefoxOptions();
+        capabilities = DesiredCapabilities.firefox();
+        FirefoxProfile profile = new FirefoxProfile();
+        profile.setPreference("general.useragent.override","Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16");
+        firefoxOptions.addArguments("--kiosk");
+        firefoxOptions.addArguments("--disable-notifications");
+        firefoxOptions.addArguments("--start-fullscreen");
+        capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+        capabilities.setCapability("marionette", true);
+        firefoxOptions.merge(capabilities);
+        profile.setPreference("general.useragent.override", "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16");
+        System.setProperty("webdriver.gecko.driver", "");
+        return firefoxOptions;
+    }
+
 
 }
