df67446c3d15447eebbbd2288ba128f55d1708fc
KYakut35 <kyakut35@gmail.com>
2025-01-11 15:05:30 +0300

    Transfer Page Functions Completed

--- src/test/java/com/qework/pages/TransferPage.java
+++ src/test/java/com/qework/pages/TransferPage.java
@@ -12,11 +12,24 @@ public class TransferPage extends BasePage {
     public TransferPage() {
     }
 
+    @Step("transferInfoTab elementi sayfada var mı kontrol et")
+    public boolean transferInfoTabIsDisplayed() {
+        logger.info(Elements.transferInfoTab + " Element located.");
+        return findElement(Elements.transferInfoTab).isDisplayed();
+    }
+
     @Step("transferMoney elementine tıkla")
     public void transferMoneyClick() {
         scrollWithAction(Elements.transferTransferMoneyButton);
         findElement(Elements.transferTransferMoneyButton).click();
         logger.info("Clicked on this element " + Elements.transferTransferMoneyButton + " .");
+    }
+
+    @Step("cardSend elementine tıkla")
+    public void cardSendClick() {
+        scrollWithAction(Elements.cardSend);
+        findElement(Elements.cardSend).click();
+        logger.info("Clicked on this element " + Elements.cardSend + " .");
     }
 
     @Step("addMoney elementine tıkla")
@@ -40,6 +53,13 @@ public class TransferPage extends BasePage {
         logger.info("Clicked on this element " + Elements.transferMoneySendButton + " .");
     }
 
+    @Step("transferBackButton elementine tıkla")
+    public void transferBackButtonSendClick() {
+        scrollWithAction(Elements.transferBackButton);
+        findElement(Elements.transferBackButton).click();
+        logger.info("Clicked on this element " + Elements.transferBackButton + " .");
+    }
+
     @Step("<text> textini transferMoneyAmount elementine yaz")
     public void transferMoneyAmount(String text) {
         scrollWithAction(Elements.transferMoneyAmount);
@@ -49,6 +69,53 @@ public class TransferPage extends BasePage {
         logger.info(text + " has been written to this element " + Elements.transferMoneyAmount + " .");
 
     }
+
+    @Step("<text> textini cardNumber elementine yaz")
+    public void cardNumber(String text) {
+        scrollWithAction(Elements.cardNumber);
+        findElement(Elements.cardNumber).click();
+        findElement(Elements.cardNumber).clear();
+        findElement(Elements.cardNumber).sendKeys(text);
+        logger.info(text + " has been written to this element " + Elements.cardNumber + " .");
+
+    }
+
+    @Step("<text> textini cardHolder elementine yaz")
+    public void cardHolder(String text) {
+        scrollWithAction(Elements.cardHolder);
+        findElement(Elements.cardHolder).click();
+        findElement(Elements.cardHolder).clear();
+        findElement(Elements.cardHolder).sendKeys(text);
+        logger.info(text + " has been written to this element " + Elements.cardHolder + " .");
+    }
+
+    @Step("<text> textini cardDate elementine yaz")
+    public void cardDate(String text) {
+        scrollWithAction(Elements.date);
+        findElement(Elements.date).click();
+        findElement(Elements.date).clear();
+        findElement(Elements.date).sendKeys(text);
+        logger.info(text + " has been written to this element " + Elements.date + " .");
+    }
+
+    @Step("<text> textini cardAmount elementine yaz")
+    public void cardAmount(String text) {
+        scrollWithAction(Elements.cardAmount);
+        findElement(Elements.cardAmount).click();
+        findElement(Elements.cardAmount).clear();
+        findElement(Elements.cardAmount).sendKeys(text);
+        logger.info(text + " has been written to this element " + Elements.cardAmount + " .");
+    }
+
+    @Step("<text> textini cardCVV elementine yaz")
+    public void cardCVV(String text) {
+        scrollWithAction(Elements.cvv);
+        findElement(Elements.cvv).click();
+        findElement(Elements.cvv).clear();
+        findElement(Elements.cvv).sendKeys(text);
+        logger.info(text + " has been written to this element " + Elements.cvv + " .");
+    }
+
 
     @Step("<text> textini transferEditAccount elementine yaz")
     public void transferEditAccount(String text) {
@@ -62,6 +129,14 @@ public class TransferPage extends BasePage {
 
     @Step("<text> textini transferMoneyAmount elementi ile karşılaştır")
     public void transferMoneyCompare(String text) {
+        WebElement element = findElement(Elements.transferCompare);
+        String compare = element.getText();
+        assertTrue(compare.equals(text));
+        logger.info(text + " textiyle " + compare + " texti karşılaştırıldı.");
+
+    }
+
+    public void addMoneyCompare(String text) {
         WebElement element = findElement(Elements.transferCompare);
         String compare = element.getText();
         assertTrue(compare.equals(text));
