a7c2e78fc054b83e82919b24fa28dc5e9814c420
KYakut35 <kyakut35@gmail.com>
2025-01-11 10:07:49 +0300

    LoginPage elements added

--- src/test/java/com/qework/elements/Elements.java
+++ src/test/java/com/qework/elements/Elements.java
@@ -4,8 +4,9 @@ import org.openqa.selenium.By;
 
 public class Elements {
 
-
-    public static By deneme = By.xpath("//button[contains(@class,'okag')]");
-
+    public static By loginIcon = By.xpath("//img[contains(@class,'css')]");
+    public static By loginUserName = By.xpath("//input[@placeholder='Username']");
+    public static By loginPassword = By.xpath("//input[@placeholder='Password']");
+    public static By loginButton = By.xpath("//div[contains(@class,'css-175oi2r r-1i6')]");
 
 }
